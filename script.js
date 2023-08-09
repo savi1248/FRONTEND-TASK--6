535095b5b7f83496c0826d4c1196d9e5d5fb2484
savitha12 <savithaa24082000@gmail.com>
2023-08-09 08:32:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const buttons = document.querySelector(".items");
+const filterImg = document.querySelectorAll(".image");
+
+window.onload=()=>{ 
+    buttons.onclick=(selectedItem)=>{
+         if(selectedItem.target.classList.contains("item")){
+             buttons.querySelector(".active").classList.remove("active");
+             selectedItem.target.classList.add("active");
+
+
+             let filterName = selectedItem.target.getAttribute("data-name");
+             filterImg.forEach((image)=>{
+                let filterImages = image.getAttribute("data-name");
+               if((filterImages == filterName)||filterName == "all"){
+                 image.classList.remove("hide");
+                 image.classList.add("show");
+               }else{
+                 image.classList.add("hide");
+                 image.classList.remove("show");
+               }
+             });
+         }
+    }
+    for(let index=0; index< filterImg.length; index++){
+        filterImg[index].setAttribute("onclick","preview(this)");
+    }
+}
+
+
+//preview fullscreen
+const previewBox = document.querySelector(".preview-box");
+previewImg = previewBox.querySelector("img"),
+categoryName = previewBox.querySelector(".title p"),
+closeIcon= previewBox.querySelector(".icon");
+shadow= document.querySelector(".shadow");
+
+
+
+function preview(element){
+    document.querySelector("body").style.overflow="hidden";
+    let selectedPrevImg = element.querySelector("img").src;
+    let selectedImgCategory = element.getAttribute("data-name");
+    categoryName.textContent = selectedImgCategory;
+    previewImg.src = selectedPrevImg;
+
+     previewBox.classList.add("show");
+     shadow.classList.add("show");
+     closeIcon.onclick=()=>{
+        previewBox.classList.remove("show");
+        shadow.classList.remove("show");
+        document.querySelector("body").style.overflow="scroll";
+    
+     }
+}
